de869471b8d09ffcf0e4101ae338cc189c739cfe
unknown <debadyuti.karmakar@gmail.com>
2022-09-30 22:55:28 +0530

    Datatype1

---
+++ Code/Datatype_var/Sum.java
@@ -0,0 +1,14 @@
+public class Sum{
+    public static void main(String[] args){
+        int a,b,c;
+        a=10;
+        b=5;
+        c=a+b;
+        System.out.println("Sum is:"+c);//15
+        //djsdvjndflv
+        /*lfljs
+        dmldsv
+        */
+        System.out.println(c);
+    }
+}
